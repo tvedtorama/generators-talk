fd8986043852577eced4202d86a1c990a4429aed
Jørgen Tvedt <tvedtorama@gmail.com>
2018-12-13 09:29:30 +0100

    Reset to talk start.

--- lib/index.ts
+++ lib/index.ts
@@ -4,21 +4,8 @@ import { createRouter } from "./routing";
 import { greetingLoop } from "./greetingLoop";
 import {Iterable} from 'ix'
 
-const gloriousStateful = function*(seed = 10) {
-	let state = seed
-
-	while (true) {
-		yield ++state
-	}
-}
-
 
 export default () => {
 
-	const gen = gloriousStateful()
-
-	for (const i of Iterable.range(0, 4)) {
-		const {value, done} = gen.next()
-		console.log(`Got value: ${value} done: ${done}`)
-	}
+	// See README for examples to paste in here
 }
